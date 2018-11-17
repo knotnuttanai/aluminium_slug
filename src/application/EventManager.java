1541990517ca350729b85a3db8fb1674789afb9d
Nuttanai Kijviwattanakarn <knot131-wr@hotmail.com>
2018-11-17 09:49:42 +0700

    click to shoot

--- src/application/EventManager.java
+++ src/application/EventManager.java
@@ -3,8 +3,7 @@ package application;
 import character.Hero;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class EventManager {
 	private Scene scene;
@@ -14,10 +13,10 @@ public class EventManager {
 		this.hero = hero;
 	}
 	public void setPlayerControl() {
-		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(KeyEvent event) {
-				if(event.getCode() == KeyCode.SPACE) hero.shoot();
+			public void handle(MouseEvent event) {
+				hero.shoot();
 			}		
 		});
 	}
